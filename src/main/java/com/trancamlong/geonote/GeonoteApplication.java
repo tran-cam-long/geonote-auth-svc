e9a46dcbbf75d336fc78e4c4d95611d32ac8b0b9
tran-cam-long <camlong.tran0702@gmail.com>
2023-10-09 19:28:00 +0700

    [GEO-001]: Init project, setup open api

---
+++ src/main/java/com/trancamlong/geonote/GeonoteApplication.java
@@ -0,0 +1,13 @@
+package com.trancamlong.geonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeonoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeonoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trancamlong/geonote/GeonoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trancamlong.geonote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeonoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

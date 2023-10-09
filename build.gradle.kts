c6c761fc7b292a54ccb8d7580fc76cbe8942d9a4
Trần Cẩm Long <75228892+tran-cam-long@users.noreply.github.com>
2023-10-10 06:43:34 +0700

    [GEO-002] Feature/geo 002 infra sonar spotless (#1)
    * [GEO-002] feature: init spotless config
    * [GEO-002] feature: set up sonar and spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,69 +1,98 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
-	id("org.springdoc.openapi-gradle-plugin") version "1.7.0"
-	id("org.openapi.generator") version "7.0.1"
+  java
+  id("org.springframework.boot") version "3.1.4"
+  id("io.spring.dependency-management") version "1.1.3"
+  // OpenAPI
+  id("org.springdoc.openapi-gradle-plugin") version "1.7.0"
+  id("org.openapi.generator") version "7.0.1"
+  // Code quality
+  id("org.sonarqube") version "4.4.1.3373"
+  id("com.diffplug.spotless") version "6.22.0"
 }
 
 group = "com.trancamlong"
+
 version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
+java { sourceCompatibility = JavaVersion.VERSION_17 }
+
+repositories { mavenCentral() }
+
+apply(plugin = "com.diffplug.spotless")
+
+spotless {
+  kotlinGradle {
+    target("**/*.gradle.kts")
+    ktfmt()
+  }
+  java {
+    targetExclude("**/generated/**/*.java")
+    removeUnusedImports()
+    importOrder()
+    googleJavaFormat()
+    endWithNewline()
+  }
 }
 
-repositories {
-	mavenCentral()
+sonar {
+  properties {
+    property("sonar.host.url", "http://localhost:9001")
+    property("sonar.projectKey", "com.trancamlong.geonote.auth")
+    property("sonar.projectName", ".geonote-auth-svc")
+    property("sonar.java.sources", "./src")
+    property("sonar.dynamicAnalysis", "reuseReports")
+    property("sonar.junit.reportsPath", "**/build/test-results/test/TEST-*.xml")
+    property("sonar.test.exclusions", "**/test/**")
+    property("sonar.coverage.exclusions", "**/model/*, **/constant/*, **/api/*")
+  }
 }
+
+tasks.build { dependsOn("spotlessApply") }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
 
-	// lombok compile and processor
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+  // lombok compile and processor
+  compileOnly("org.projectlombok:lombok")
+  annotationProcessor("org.projectlombok:lombok")
 
-	// OpenAPI
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+  // OpenAPI
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 
-	implementation("org.slf4j:slf4j-log4j12:1.7.25")
-	implementation("org.slf4j:slf4j-jdk14:1.7.25")
+  implementation("org.slf4j:slf4j-jdk14:1.7.25")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+tasks.withType<Test> { useJUnitPlatform() }
 
 tasks.openApiGenerate {
-	generatorName.set("spring")
-	inputSpec.set("$projectDir/src/main/resources/static/api.yaml")
-	outputDir.set("$buildDir/generated/openapi")
-	apiPackage.set("com.trancamlong.geonote.api")
-	modelPackage.set("com.trancamlong.geonote.model")
-	configOptions.set(
-			mapOf(
-					"dateLibrary" to "java8",
-					"generateApis" to "true",
-					"generateApiTests" to "false",
-					"generateModels" to "true",
-					"generateModelTests" to "false",
-					"generateModelDocumentation" to "false",
-					"generateSupportingFiles" to "false",
-					"hideGenerationTimestamp" to "true",
-					"interfaceOnly" to "true",
-					"library" to "spring-boot",
-					"serializableModel" to "true",
-					"useBeanValidation" to "true",
-					"useTags" to "true",
-					"implicitHeaders" to "true",
-					"openApiNullable" to "false",
-					"oas3" to "true",
-					"useSpringBoot3" to "true"))
+  generatorName.set("spring")
+  inputSpec.set("$projectDir/src/main/resources/static/api.yaml")
+  outputDir.set("$buildDir/generated/openapi")
+  apiPackage.set("com.trancamlong.geonote.api")
+  modelPackage.set("com.trancamlong.geonote.model")
+  configOptions.set(
+      mapOf(
+          "dateLibrary" to "java8",
+          "generateApis" to "true",
+          "generateApiTests" to "false",
+          "generateModels" to "true",
+          "generateModelTests" to "false",
+          "generateModelDocumentation" to "false",
+          "generateSupportingFiles" to "false",
+          "hideGenerationTimestamp" to "true",
+          "interfaceOnly" to "true",
+          "library" to "spring-boot",
+          "serializableModel" to "true",
+          "useBeanValidation" to "true",
+          "useTags" to "true",
+          "implicitHeaders" to "true",
+          "openApiNullable" to "false",
+          "oas3" to "true",
+          "useSpringBoot3" to "true"))
 }
 
 sourceSets { main { java { srcDirs("$buildDir/generated/openapi/src/main/java") } } }
--- src/main/java/com/trancamlong/geonote/GeonoteApplication.java
+++ src/main/java/com/trancamlong/geonote/GeonoteApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeonoteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeonoteApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GeonoteApplication.class, args);
+  }
 }
